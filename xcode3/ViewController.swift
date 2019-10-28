b5424f25c76b3e52989f84c7c6aac76199fac2cb
nguyenthanhvu97 <nguyenthanhvu908@gmail.com>
2019-10-28 22:42:10 +0700

    add xcode3 add xcode3.xcodeproj

---
+++ xcode3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  xcode3
+//
+//  Created by Nguyễn Thanh  Vũ on 10/28/19.
+//  Copyright © 2019 thanhvu97. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var person = "yuri"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
